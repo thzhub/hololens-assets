9a1ca008062d641c8c9b74fd497b75c7fdc2b4e6
thzhub <tianhzhu@gmail.com>
2017-03-17 22:40:28 -0400

    add menu

---
+++ Assets/AssetDatabase/menu/CategoryMenuLoader.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CategoryMenuLoader : MonoBehaviour {
+
+    public GameObject spawner;
+    public string category;
+    private List<GameObject> furnitures;
+
+	void Start () {
+        SpawnList spawnList = spawner.GetComponent<SpawnList>();
+        furnitures = spawnList.findBundle(category);
+
+        int i = 0;
+        foreach (Transform child in transform)
+        {
+            if (child.gameObject.name.Equals("Button"))
+            {
+                if (i < furnitures.Count)
+                {
+                    GameObject f = furnitures[i];
+                    FurnitureProperty properties = spawnList.findAssetProperties(category, f.name);
+                    child.gameObject.GetComponent<FurnitureMenuItemProperty>().furnitureProperty.bundle = properties.bundle;
+                    child.gameObject.GetComponent<FurnitureMenuItemProperty>().furnitureProperty.assetName = properties.assetName;
+                    child.gameObject.GetComponent<FurnitureMenuItemProperty>().furnitureProperty.furnitureType = properties.furnitureType;
+                    child.gameObject.GetComponent<Image>().sprite = spawnList.findAssetIcon(category, i);
+                    i++;
+                }
+                else
+                {
+                    child.gameObject.SetActive(false);
+                }
+            }
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/editor/MenuCreator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class MenuCreator : MonoBehaviour {
+
+    [MenuItem("GameObject/FurnitureMenu/Category", false, 0)]
+    static void CreateCategoryMenu(MenuCommand command)
+    {
+        Debug.Log("Category Menu");
+        GameObject assetSpawner = (GameObject)command.context;
+        List<GameObject> spawnList  = GetSpawnList(assetSpawner, "chairs");
+    }
+
+    static List<GameObject> GetSpawnList(GameObject spawner, string category)
+    {
+        switch (category)
+        {
+            case "chairassets":
+                return spawner.GetComponent<SpawnList>().chairs;
+            case "drawerassets":
+                return spawner.GetComponent<SpawnList>().drawers;
+            case "tableassets":
+                return spawner.GetComponent<SpawnList>().tables;
+            case "sofaassets":
+                return spawner.GetComponent<SpawnList>().sofas;
+            case "bedassets":
+                return spawner.GetComponent<SpawnList>().beds;
+            case "storageassets":
+                return spawner.GetComponent<SpawnList>().storage;
+            case "lightassets":
+                return spawner.GetComponent<SpawnList>().lights;
+            default:
+                return new List<GameObject>();
+        }
+    }
+}
